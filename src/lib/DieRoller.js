04a980145217617255b87db5b0dd5fa410a96b3b
Phil Guinchard <phil.guinchard@gmail.com>
2018-02-14 01:01:30 -0500

    Adding in the DieRoller lib

---
+++ src/lib/DieRoller.js
@@ -0,0 +1,38 @@
+export default class DieRoller {
+	constructor() {
+		this.validLastRollTypes = ['skillCheck', 'toHit', 'normalDamage', 'killingDamage'];
+	}
+	
+	rollCheck() {
+		return roll(3, 'skillCheck');
+	}
+	
+	_roll(dice, rollType, partialDieType) {
+		let resultRoll = {
+			rollType: rollType,
+			total: 0,
+			rolls: [],
+			partialDieType: partialDieType || null
+		};
+		let roll = 0;
+		lastRollType = validLastRollTypes.indexOf(rollType) !== -1 ? rollType : lastRollType;
+		
+		for (let i = 0; i < dice; i++) {
+			roll = Math.floor(Math.random() * 6) + 1;
+			
+			resultRoll.total += roll;
+			resultRoll.rolls.push(roll);
+		}
+		
+		if (partialDieType === 'plusOne') {
+			resultRoll.total += 1;
+		} else if (partialDieType === 'halfDie') {
+			let halfDie = Math.floor(Math.random() * 3) + 1;
+			
+			resultRoll.total += halfDie;
+			resultRoll.rolls.push(halfDie);
+		}				
+		//console.dir(resultRoll);
+		return resultRoll;
+	}
+}
