a0fb823fe1f573976e7fa52d6701339c74311d8a
Phil Guinchard <phil.guinchard@gmail.com>
2018-02-26 16:25:10 -0800

    Making an exception for an iPad rendering issue

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,21 @@
 import React, { Component }  from 'react';
-import { Platform, StyleSheet, View, Image, TouchableHighlight } from 'react-native';
+import { Dimensions, Platform, StyleSheet, View, Image, TouchableHighlight } from 'react-native';
 import { Button, Text, Header, Left, Right, Icon } from 'native-base';
 
 export default class MyHeader extends Component {
+	_renderSpacer() {
+	    let {height, width} = Dimensions.get('window');
+
+	    if (Platform.OS === 'ios' && height / width <= 1.6) {
+		return <View style={{paddingBottom: 20}} />
+	    }
+
+	    return null;
+	}
+
 	render() {
 		return (
+			<View>
 			<Header hasTabs={this.props.hasTabs || false} style={localStyles.header}>
 		  	  <Left>
 				<View style={localStyles.logo}>
@@ -18,7 +29,9 @@ export default class MyHeader extends Component {
 	              <Icon name='menu' style={{color: 'white', paddingBottom: Platform.OS === 'ios' ? 15 : 0}} />
 	            </Button>
 	          </Right>
-	        </Header>	
+	        </Header>
+		{this._renderSpacer()}	
+		</View>
 		);
 	}
 }
