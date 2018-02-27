b0418749ad6d93c49c99ec3e24cb757e750c9247
Phil Guinchard <phil.guinchard@gmail.com>
2018-02-26 16:28:03 -0800

    Code formatting

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,7 +7,7 @@ export default class MyHeader extends Component {
 	    let {height, width} = Dimensions.get('window');
 
 	    if (Platform.OS === 'ios' && height / width <= 1.6) {
-		return <View style={{paddingBottom: 20}} />
+		    return <View style={{paddingBottom: 20}} />
 	    }
 
 	    return null;
@@ -16,22 +16,22 @@ export default class MyHeader extends Component {
 	render() {
 		return (
 			<View>
-			<Header hasTabs={this.props.hasTabs || false} style={localStyles.header}>
-		  	  <Left>
-				<View style={localStyles.logo}>
-				    <TouchableHighlight underlayColor='#3da0ff' onPress={() => this.props.navigation.navigate('Home')}>
-					    <Image source={require('../../../public/hero_logo.png')} />
-					</TouchableHighlight>
-				</View>
-		  	  </Left>
-	          <Right>
-	            <Button transparent onPress={() => this.props.navigation.navigate("DrawerOpen")}>
-	              <Icon name='menu' style={{color: 'white', paddingBottom: Platform.OS === 'ios' ? 15 : 0}} />
-	            </Button>
-	          </Right>
-	        </Header>
-		{this._renderSpacer()}	
-		</View>
+                <Header hasTabs={this.props.hasTabs || false} style={localStyles.header}>
+                  <Left>
+                    <View style={localStyles.logo}>
+                        <TouchableHighlight underlayColor='#3da0ff' onPress={() => this.props.navigation.navigate('Home')}>
+                            <Image source={require('../../../public/hero_logo.png')} />
+                        </TouchableHighlight>
+                    </View>
+                  </Left>
+                  <Right>
+                    <Button transparent onPress={() => this.props.navigation.navigate("DrawerOpen")}>
+                      <Icon name='menu' style={{color: 'white', paddingBottom: Platform.OS === 'ios' ? 15 : 0}} />
+                    </Button>
+                  </Right>
+                </Header>
+		        {this._renderSpacer()}
+		    </View>
 		);
 	}
 }
@@ -46,7 +46,7 @@ const localStyles = StyleSheet.create({
 		alignSelf: 'center',
 		...Platform.select({
 		    ios: {
-			paddingBottom: 20
+			    paddingBottom: 20
 		    }
 		})
 	}
