acfb266e785e7f1df79c40f70e6ecfdc526bd67d
Phil Guinchard <phil.guinchard@gmail.com>
2020-01-03 14:12:21 -0800

    Upgrading react-navigation libs

--- App.js
+++ App.js
@@ -1,77 +1,16 @@
 import React, { Component }  from 'react';
 import { StyleSheet, Text, View, Button, StatusBar, Alert } from 'react-native';
-import { DrawerNavigator } from 'react-navigation';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
+import SplashScreen from 'react-native-splash-screen'
 import thunk from 'redux-thunk';
 import { Root } from "native-base";
-import SplashScreen from 'react-native-splash-screen'
-import HomeScreen from './src/components/Screens/HomeScreen';
-import ViewCharacterScreen from './src/components/Screens/ViewCharacterScreen';
-import ViewHeroDesignerCharacterScreen from './src/components/Screens/ViewHeroDesignerCharacterScreen';
-import RandomCharacterScreen from './src/components/Screens/RandomCharacterScreen';
-import PdfViewerScreen from './src/components/Screens/PdfViewerScreen';
-import ResultScreen from './src/components/Screens/ResultScreen';
-import SkillScreen from './src/components/Screens/SkillScreen';
-import HitScreen from './src/components/Screens/HitScreen';
-import DamageScreen from './src/components/Screens/DamageScreen';
-import FreeFormScreen from './src/components/Screens/FreeFormScreen';
-import CostCruncherScreen from './src/components/Screens/CostCruncherScreen';
-import StatisticsScreen from './src/components/Screens/StatisticsScreen';
-import SettingsScreen from './src/components/Screens/SettingsScreen';
-import Sidebar from './src/components/Sidebar/Sidebar';
 import { statistics } from './src/lib/Statistics';
 import { heroDesignerCharacter } from './src/lib/HeroDesignerCharacter';
 import { common } from './src/lib/Common';
 import reducer from './src/reducers/index';
 import { GET_SHOW_SECONDARY } from './src/reducers/character';
-
-const RootStack = DrawerNavigator({
-		Home: {
-			screen: HomeScreen,
-		},
-		ViewCharacter: {
-			screen: ViewCharacterScreen
-		},
-		ViewHeroDesignerCharacter: {
-			screen: ViewHeroDesignerCharacterScreen
-		},
-		RandomCharacter: {
-			screen: RandomCharacterScreen
-		},
-		PdfViewer: {
-			screen: PdfViewerScreen
-		},
-		Result: {
-			screen: ResultScreen
-		},
-		Skill: {
-			screen: SkillScreen
-		},
-		Hit: {
-			screen: HitScreen
-		},
-		Damage: {
-			screen: DamageScreen
-		},
-		FreeForm: {
-			screen: FreeFormScreen
-		},
-		CostCruncher: {
-            screen: CostCruncherScreen
-        },
-		Statistics: {
-		    screen: StatisticsScreen
-		},
-		Settings: {
-			screen: SettingsScreen
-		}
-	}, {
-		initialRouteName: 'Home',
-		drawerPosition: 'right',
-		contentComponent: props => <Sidebar {...props} />
-	}
-);
+import AppNavigator from './AppNavigator';
 
 export const store = createStore(reducer, applyMiddleware(thunk));
 
@@ -84,7 +23,7 @@ export default class App extends Component {
 		return (
 			<Provider store={store}>
 				<Root>
-					<RootStack />
+					<AppNavigator />
 				</Root>
 			</Provider>
 		);
---
+++ AppNavigator.js
@@ -0,0 +1,37 @@
+import React, { Component }  from 'react';
+import { createAppContainer } from 'react-navigation';
+import { createDrawerNavigator } from 'react-navigation-drawer';
+import HomeScreen from './src/components/Screens/HomeScreen';
+import ViewCharacterScreen from './src/components/Screens/ViewCharacterScreen';
+import ViewHeroDesignerCharacterScreen from './src/components/Screens/ViewHeroDesignerCharacterScreen';
+import RandomCharacterScreen from './src/components/Screens/RandomCharacterScreen';
+import ResultScreen from './src/components/Screens/ResultScreen';
+import SkillScreen from './src/components/Screens/SkillScreen';
+import HitScreen from './src/components/Screens/HitScreen';
+import DamageScreen from './src/components/Screens/DamageScreen';
+import FreeFormScreen from './src/components/Screens/FreeFormScreen';
+import CostCruncherScreen from './src/components/Screens/CostCruncherScreen';
+import StatisticsScreen from './src/components/Screens/StatisticsScreen';
+import SettingsScreen from './src/components/Screens/SettingsScreen';
+import Sidebar from './src/components/Sidebar/Sidebar';
+
+const AppNavigator = createDrawerNavigator({
+		Home: HomeScreen,
+		ViewCharacter: ViewCharacterScreen,
+		ViewHeroDesignerCharacter: ViewHeroDesignerCharacterScreen,
+		RandomCharacter: RandomCharacterScreen,
+		Result: ResultScreen,
+		Skill: SkillScreen,
+		Hit: HitScreen,
+		Damage: DamageScreen,
+		FreeForm: FreeFormScreen,
+		CostCruncher: CostCruncherScreen,
+		Statistics: StatisticsScreen,
+		Settings: SettingsScreen
+}, {
+    initialRouteName: 'Home',
+    drawerPosition: 'right',
+    contentComponent: Sidebar
+});
+
+export default createAppContainer(AppNavigator);
