0ac448c9bdd9dffd7c9ab9f87dc2fac7fed6a300
Phil Guinchard <phil.guinchard@gmail.com>
2020-01-11 08:41:06 -0500

    Adding in indentation for switch statements

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,7 @@ module.exports = {
   "root": true,
   "extends": '@react-native-community',
   "rules": {
-    "indent": ["error", 4],
+    "indent": ["error", 4, {"SwitchCase": 1}],
     "no-inline-styles": 1,
     "prettier/prettier": 0,
   }
