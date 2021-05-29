79493b9faa57144fa0efd28ef3d2d2d942edcbac
Phil Guinchard <phil.guinchard@gmail.com>
2021-05-29 04:04:45 -0400

    Fixed a spacing issue with the app header

--- src/Styles.js
+++ src/Styles.js
@@ -1,5 +1,4 @@
 import {ScaledSheet} from 'react-native-size-matters';
-import {ifIphoneX} from 'react-native-iphone-x-helper';
 
 // Copyright 2018-Present Philip J. Guinchard
 //
@@ -18,13 +17,11 @@ import {ifIphoneX} from 'react-native-iphone-x-helper';
 export default ScaledSheet.create({
     container: {
         backgroundColor: '#1b1d1f',
-        ...ifIphoneX({
-            paddingTop: '50@vs',
-        }),
     },
     content: {
         paddingTop: 0,
         paddingHorizontal: 0,
+        backgroundColor: '#1b1d1f',
     },
     heading: {
         fontSize: '20@vs',
